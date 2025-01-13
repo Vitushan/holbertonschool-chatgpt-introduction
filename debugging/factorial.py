9b43826b829de1a785734796e8a5f28da8199874
Vitushan <vitushan@outlook.fr>
2025-01-13 10:10:08 +0100

    fixing infinity loop

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,23 @@
 import sys
 
 def factorial(n):
+
     result = 1
     while n > 1:
         result *= n
         n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+
+if len(sys.argv) > 1:
+    try:
+        number = int(sys.argv[1])
+        if number < 0:
+            print("Error : the factorial is not define for the negative numbers.")
+        else:
+            f = factorial(number)
+            print(f)
+    except ValueError:
+        print("Error : stdin is a integer.")
+else:
+    print("Usage : ./factorial.py <integer_positif>")
